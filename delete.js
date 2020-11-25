c00738b57ede6248135833792b11abc21bc8088a
val8119 <48407846+val8119@users.noreply.github.com>
2020-11-25 10:37:36 +0200

    Removed comments

--- delete.js
+++ delete.js
@@ -1,11 +1,8 @@
 var checker = setInterval(function () {
-    console.log("checked")
 
     if (document.querySelector('.adblock-notif') != null) {
         element = document.querySelector('.adblock-notif');
         element.parentNode.removeChild(element);
-
-        console.log("deleted")
 
         clearInterval(checker);
     }
